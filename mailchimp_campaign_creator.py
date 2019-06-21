11cbbd991483164d569af0cf1b03d2ac9a550a97
Rafasmafa <nrafanelli@gmail.com>
2019-06-20 20:34:21 -0400

    added --headless option to chromedriver

--- mailchimp_campaign_creator.py
+++ mailchimp_campaign_creator.py
@@ -114,6 +114,7 @@ class MailChimpCampaignCreator(object):
                             os.environ['GOOGLE_CHROME_BIN']
                         chrome_options.add_argument('--disable-gpu')
                         chrome_options.add_argument('--no-sandbox')
+                        chrome_options.add_argument('--headless')
                         chrome_options.add_argument('--disable-dev-shm-usage')
                         driver = webdriver.Chrome(
                             executable_path=str(os.environ.get('CHROMEDRIVER_PATH')),
